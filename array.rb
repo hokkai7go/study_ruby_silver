6ab2c1ca6d8c24c23fea2bf7b5297df51f72e056
Yutaro Sugai <y.sugai@everyleaf.com>
2012-09-25 20:06:35 +0900

    Arrayクラスで遊んでみる

---
+++ array.rb
@@ -0,0 +1,18 @@
+String
+String.superclass
+String.superclass.superclass
+Array
+[1, 5, 9]
+[1, 5, 9].class
+ary = [1, 5, 9]
+ary.all
+ary.all?
+ary.all? {|v| v> 0}
+ary.all? {|v| v > 0}
+ary.all? {|v| v < 0}
+bry = [1, nil, 4]
+bry.all?
+bry = [1, false, 4]
+bry.all?
+ary.map {|n| n**2 / 2}
+ary.map {|n| n**2 / 2.0}
