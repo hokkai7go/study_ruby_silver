4d58fad23167d564ca64c18058a8df240a644792
Yutaro Sugai <y.sugai@everyleaf.com>
2012-09-26 09:59:19 +0900

    ひたすらにるりまを見て、Arrayクラスのメソッドを呼んでいる

--- array.rb
+++ array.rb
@@ -16,3 +16,37 @@ bry = [1, false, 4]
 bry.all?
 ary.map {|n| n**2 / 2}
 ary.map {|n| n**2 / 2.0}
+cry = []
+cry.cycle
+cry.cycle()
+dry.cycle
+cry = [1, 2, 3, 4, 5, 6, 7, 8, 8]
+cry.find
+cry.find(2)
+cry.find {|i| i > 3}
+cry.detect {|i| i > 3}
+cry.detect(ifnone = nil) {|i| i > 3}
+cry.detect(ifnone = nil) {|i| i > 100}
+cry
+cry.drip
+cry.drop(4)
+ary & bry
+ary
+bry
+cry
+ary & cry
+ary.sep
+ary.sep(*)
+ary.sep('*')
+Array#sep
+$ Array#sep
+? Array#sep
+? Array#*
+$ Array#*
+ary * 3
+ary + 2
+ary + [2]
+ary + [3]
+ary
+ary - [5]
+? Object#eql?
